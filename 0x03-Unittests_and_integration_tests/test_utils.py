bbae5dde0df0c936a0189d13258e8dbcc0b08eea
pizzosta <pizzostor@gmail.com>
2023-09-01 00:48:45 +0100

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,9 +2,9 @@
 """A module for testing the utils module."""
 import unittest
 from typing import Any, Dict, Tuple
-from parameterized import parameterized
 from unittest.mock import patch, Mock
-from utils import access_nested_map, get_json
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -59,3 +59,27 @@ class TestGetJson(unittest.TestCase):
 
         mock_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Tests the utils.memoize function."""
+
+    def test_memoize(self) -> None:
+        """Tests the utils.memoize function output."""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=lambda: 42) as mock_method:
+            test_class = TestClass()
+
+            result1 = test_class.a_property()
+            result2 = test_class.a_property()
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock_method.assert_called_once()
