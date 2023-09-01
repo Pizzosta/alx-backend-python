26a9dc9240bd60ec7bad84933efaa69e9c36f39b
pizzosta <pizzostor@gmail.com>
2023-09-01 13:45:22 +0000

    Parameterize

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -48,6 +48,16 @@ class TestGithubOrgClient(unittest.TestCase):
             "https://api.github.com/users/google/repos",
         )
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
+        """Tests the has_license method with parameterization."""
+        github_client = GithubOrgClient("google")
+        result = github_client.has_license(repo, license_key)
+        self.assertEqual(result, expected_result)
+
 
 if __name__ == '__main__':
     unittest.main()
